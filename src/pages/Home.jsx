b721f34155d36a00a855f816026fd2b98fc575c8
Ashutosh Uniyal <ashutoshuniyal012@gmail.com>
2023-10-31 12:58:02 +0530

    added required file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import { Hero } from "../components/Hero";
+import { Courses } from "../components/Courses";
+import { Project } from "../components/Project";
+import { Testimonial } from "../components/Testimonial";
+import { Footer } from "../components/Footer";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Navbar />
+      <Hero />
+      <Courses />
+      <Project />
+      <Testimonial />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
